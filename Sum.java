c6ee096a43ac74928e5fb4a5e0d4e532fc0b3ad0
daaa08 <daaa08@naver.com>
2017-05-29 20:39:34 +0900

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// 1~100까지 더하기
+		
+		int sum = 0;
+		for(int i = 1; i <=100; i ++){
+			sum += i;
+		}
+		System.out.print(sum);
+	}
+
+}
