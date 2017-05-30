82e35846cdf19be494959b54ea59964eb77543fe
daaa08 <daaa08@naver.com>
2017-05-30 23:52:14 +0900

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,13 @@
+
+
+public class Random {
+
+	public static void main(String[] args) {
+		// 랜덤으로 1~10까지 숫자 중 하나 출력하기
+		
+		int a;		
+		Random random = new Random();
+		System.out.println("랜덤으로" + a + "이(가) 출력되었습니다.");
+
+	}
+}
